2ffb4a2cc502cc267cf6c1673943610df3f4b0df
Jianwei Jiang <a370109945@gmail.com>
2019-08-25 00:36:47 +0800

    Fix cmake and add png++, make it run on mac

--- src/main.cpp
+++ src/main.cpp
@@ -59,6 +59,9 @@ int main(){
 	glfwWindowHint(GLFW_SAMPLES, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// Open a window and create its OpenGL context
